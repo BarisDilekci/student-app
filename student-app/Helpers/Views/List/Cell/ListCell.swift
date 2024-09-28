a2b72e5ab4955ca517820b86257fff2099428457
BarisDilekci <bdilekci200@gmail.com>
2024-09-28 22:44:26 +0300

    feat: start project

--- student-app/Helpers/Views/List/Cell/ListCell.swift
+++ student-app/Helpers/Views/List/Cell/ListCell.swift
@@ -8,11 +8,34 @@
 import SwiftUI
 
 struct ListCell: View {
+    
+    @State var name: String
+    @State var tc_number: String
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack {
+            VStack(alignment: .leading) {
+                Text(name)
+                    .font(.headline)
+                    .fontWeight(.bold)
+                    .padding(.bottom, 2)
+                
+                Text(tc_number)
+                    .font(.subheadline)
+                    .foregroundColor(.gray)
+            }
+            .padding()
+            
+            Spacer()
+        }
+        .background(Color(.systemGray6))
+        .cornerRadius(10)
+        .shadow(color: Color.black.opacity(0.1), radius: 5, x: 0, y: 2)
+        .padding(.horizontal)
     }
 }
 
 #Preview {
-    ListCell()
+    ListCell(name: "John Doe", tc_number: "131312")
+        .previewLayout(.sizeThatFits)
 }
