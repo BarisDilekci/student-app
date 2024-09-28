23c09adf663e4d226a3987cb9a62b6973d5540b8
BarisDilekci <bdilekci200@gmail.com>
2024-09-28 22:44:14 +0300

    first commit

---
+++ student-app/Helpers/Views/List/Cell/ListCell.swift
@@ -0,0 +1,18 @@
+//
+//  ListCell.swift
+//  student-app
+//
+//  Created by Barış Dilekçi on 28.09.2024.
+//
+
+import SwiftUI
+
+struct ListCell: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ListCell()
+}
