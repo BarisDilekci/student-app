a9e5b705bb4331717baecb6400a58df3f7f739e2
BarisDilekci <bdilekci200@gmail.com>
2024-09-28 22:36:50 +0300

    Initial Commit

---
+++ student-app/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  student-app
+//
+//  Created by Barış Dilekçi on 28.09.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ student-app/student_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  student_appApp.swift
+//  student-app
+//
+//  Created by Barış Dilekçi on 28.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct student_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
